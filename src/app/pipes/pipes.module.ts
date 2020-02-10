393e750b338ae0eead037b4366cb0308559e5ca7
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-02-10 15:38:28 +0000

    feat(pipes): add pipes

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core'
 
+import { AmountConverterPipe } from './amount-converter/amount-converter.pipe'
+import { FeeConverterPipe } from './fee-converter/fee-converter.pipe'
 import { ShortenStringPipe } from './shorten-string/shorten-string.pipe'
 
 @NgModule({
-  declarations: [ShortenStringPipe],
+  declarations: [ShortenStringPipe, AmountConverterPipe, FeeConverterPipe],
   imports: [],
-  exports: [ShortenStringPipe]
+  exports: [ShortenStringPipe, AmountConverterPipe, FeeConverterPipe]
 })
 export class PipesModule {}
