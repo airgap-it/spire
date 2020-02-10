ed2165c003b79729cca8b3de855908b1a68cdf68
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-02-10 15:39:53 +0000

    feat(components): update components module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,13 +2,17 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { IonicModule } from '@ionic/angular'
+import { MomentModule } from 'ngx-moment'
 
+import { PipesModule } from '../pipes/pipes.module'
+
+import { AddressRowComponent } from './address-row/address-row.component'
 import { FromToComponent } from './from-to/from-to.component'
 import { IdenticonComponent } from './identicon/identicon.component'
 
 @NgModule({
-  declarations: [IdenticonComponent, FromToComponent],
-  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
-  exports: [IdenticonComponent, FromToComponent]
+  declarations: [IdenticonComponent, FromToComponent, AddressRowComponent],
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule, PipesModule, MomentModule],
+  exports: [IdenticonComponent, FromToComponent, AddressRowComponent]
 })
 export class ComponentsModule {}
