195c6714d573ff99a9a092376cf1bd08a954952a
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-02-10 14:04:41 +0000

    feat(beacon): use qr data from background.js

--- src/app/pages/add-wallet-connection/add-wallet-connection.page.ts
+++ src/app/pages/add-wallet-connection/add-wallet-connection.page.ts
@@ -1,27 +1,15 @@
-import { Component, NgZone, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 import { ModalController } from '@ionic/angular'
-
-import { CommunicationService } from '../../services/communication.service'
 
 @Component({
   selector: 'beacon-add-wallet-connection',
   templateUrl: './add-wallet-connection.page.html',
   styleUrls: ['./add-wallet-connection.page.scss']
 })
-export class AddWalletConnectionPage implements OnInit {
+export class AddWalletConnectionPage {
   public handshakeData: string | undefined
 
-  constructor(
-    private readonly communicationService: CommunicationService,
-    private readonly modalController: ModalController,
-    private readonly ngZone: NgZone
-  ) {}
-
-  public async ngOnInit(): Promise<void> {
-    this.handshakeData = await this.communicationService.getQrData()
-    await this.ngZone.run(async () => {}) // Trigger change detection
-    console.log(this.handshakeData)
-  }
+  constructor(private readonly modalController: ModalController) {}
 
   public async dismiss(): Promise<void> {
     await this.modalController.dismiss()
