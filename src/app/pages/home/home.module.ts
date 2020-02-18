6accf063d2fa9b81467532c0c103599231837765
Pascal Brun <brunpasc@gmail.com>
2020-02-18 14:07:01 +0100

    feat(app): consistent styling

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,20 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular'
 
-import { HomePageRoutingModule } from './home-routing.module';
+import { HomePageRoutingModule } from './home-routing.module'
+import { ComponentsModule } from '../../components/components.module'
 
-import { HomePage } from './home.page';
+import { HomePage } from './home.page'
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule, ComponentsModule],
   declarations: [HomePage]
 })
 export class HomePageModule {}
