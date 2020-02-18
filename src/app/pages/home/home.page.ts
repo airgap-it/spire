948c9aaf4e1a4a31cf2b0f08817ce4add599af3b
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-02-18 10:34:43 +0000

    feat(local): show balance and address

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core'
 import { ModalController } from '@ionic/angular'
+import { AirGapMarketWallet } from 'airgap-coin-lib'
+import { LocalWalletService } from 'src/app/services/local-wallet.service'
 import { SigningMethodService } from 'src/app/services/signing-method.service'
 import { StorageKey, StorageService } from 'src/app/services/storage.service'
 
 import { PairPage } from '../pair/pair.page'
+
+enum SigningMethods {
+  WALLET = 'WALLET',
+  LEDGER = 'LEDGER',
+  LOCAL_MNEMONIC = 'LOCAL_MNEMONIC'
+}
 
 @Component({
   selector: 'app-home',
@@ -11,18 +19,22 @@ import { PairPage } from '../pair/pair.page'
   styleUrls: ['./home.page.scss']
 })
 export class HomePage {
+  public signingMethods: typeof SigningMethods = SigningMethods
   public currentSigningMethod: string = 'Unpaired'
 
   constructor(
+    public readonly localWalletService: LocalWalletService,
     private readonly signingMethodService: SigningMethodService,
     private readonly modalController: ModalController,
     private readonly storageService: StorageService
   ) {
     this.signingMethodService.signingMethod.asObservable().subscribe(method => {
-      if (method === 'WALLET') {
+      if (method === SigningMethods.WALLET) {
         this.currentSigningMethod = 'Wallet'
-      } else if (method === 'LOCAL_MNEMONIC') {
+      } else if (method === SigningMethods.LOCAL_MNEMONIC) {
         this.currentSigningMethod = 'Local Secret'
+      } else if (method === SigningMethods.LEDGER) {
+        this.currentSigningMethod = 'Ledger'
       } else {
         this.currentSigningMethod = method
       }
@@ -45,6 +57,12 @@ export class HomePage {
     if (!hasOnboarded) {
       await this.showPairPage()
       await this.storageService.set(StorageKey.HAS_ONBOARDED, true)
+    }
+  }
+
+  public getBalance(wallet: AirGapMarketWallet | undefined): void {
+    if (wallet) {
+      return wallet.currentBalance
     }
   }
 }
