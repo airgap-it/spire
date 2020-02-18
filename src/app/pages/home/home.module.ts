eb8df7bdbfd65b013d3ddc972bf84d8d4f21a0c6
Andreas Gassmann <andreas@andreasgassmann.ch>
2020-02-18 12:15:02 +0000

    feat(home): add currency pipe to balance

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,20 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { IonicModule } from '@ionic/angular'
+import { PipesModule } from 'src/app/pipes/pipes.module'
 
-import { IonicModule } from '@ionic/angular';
-
-import { HomePageRoutingModule } from './home-routing.module';
-
-import { HomePage } from './home.page';
+import { HomePageRoutingModule } from './home-routing.module'
+import { HomePage } from './home.page'
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule, PipesModule],
   declarations: [HomePage]
 })
 export class HomePageModule {}
