8c76cb0a0abbd6796d0823d19d27ea4328632352
AndreasGassmann <andreas@andreasgassmann.ch>
2020-04-30 12:33:29 +0000

    feat(dependencies): remove unused dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common'
 import { ErrorHandler, NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouteReuseStrategy } from '@angular/router'
-import { SplashScreen } from '@ionic-native/splash-screen/ngx'
-import { StatusBar } from '@ionic-native/status-bar/ngx'
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular'
 import { MomentModule } from 'ngx-moment'
 
@@ -18,8 +16,6 @@ import { PipesModule } from './pipes/pipes.module'
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ComponentsModule, PipesModule, MomentModule],
   providers: [
-    StatusBar,
-    SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     { provide: LocationStrategy, useClass: HashLocationStrategy },
     { provide: ErrorHandler, useClass: SentryErrorHandler }
