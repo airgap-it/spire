fcb115e27c7581a8b292321ec9be1b02aee86c42
Pascal Brun <brunpasc@gmail.com>
2020-05-01 12:01:51 +0200

    feat(permission-list): styling & refactoring

--- src/app/pages/permission-list/permission-list.module.ts
+++ src/app/pages/permission-list/permission-list.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { IonicModule } from '@ionic/angular'
 
+import { ComponentsModule } from '../../components/components.module'
 import { PermissionListPageRoutingModule } from './permission-list-routing.module'
 import { PermissionListPage } from './permission-list.page'
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, PermissionListPageRoutingModule],
+  imports: [CommonModule, FormsModule, IonicModule, PermissionListPageRoutingModule, ComponentsModule],
   declarations: [PermissionListPage]
 })
 export class PermissionListPageModule {}
