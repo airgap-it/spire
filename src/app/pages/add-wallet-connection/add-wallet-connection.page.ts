46baa3ea43c9cdf314f92db45455ade4d5c0bd74
Andreas Gassmann <andreas@andreasgassmann.ch>
2021-02-03 15:39:23 +0000

    feat(qr): copy code to clipboard

--- src/app/pages/add-wallet-connection/add-wallet-connection.page.ts
+++ src/app/pages/add-wallet-connection/add-wallet-connection.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core'
-import { ModalController } from '@ionic/angular'
+import { ModalController, ToastController } from '@ionic/angular'
 
 @Component({
   selector: 'beacon-add-wallet-connection',
@@ -9,9 +9,28 @@ import { ModalController } from '@ionic/angular'
 export class AddWalletConnectionPage {
   public handshakeData: string | undefined
 
-  constructor(private readonly modalController: ModalController) {}
+  constructor(private readonly modalController: ModalController, private readonly toastController: ToastController) { }
 
   public async dismiss(): Promise<void> {
     await this.modalController.dismiss()
+  }
+
+  public async copyToClipboard(): Promise<void> {
+    console.log(this.handshakeData)
+    navigator.clipboard.writeText(this.handshakeData || '').then(
+      async () => {
+        const toast = await this.toastController.create({
+          header: 'Success',
+          message: 'Code was copied to clipboard!',
+          duration: 3000
+        })
+        toast.present()
+
+        console.log('Copying to clipboard was successful!')
+      },
+      err => {
+        console.error('Could not copy text to clipboard: ', err)
+      }
+    )
   }
 }
