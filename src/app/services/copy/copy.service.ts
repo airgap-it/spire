a62e9e6e33f100dcaa4a42a72bc1eb8ee78a3280
Mike Godenzi <m.godenzi@papers.ch>
2021-08-25 08:27:59 +0200

    fix(copy): using simple method to copy to clipboard

--- src/app/services/copy/copy.service.ts
+++ src/app/services/copy/copy.service.ts
@@ -6,18 +6,18 @@ import { Injectable } from '@angular/core'
 export class CopyService {
   public copyToClipboard(val: string) {
     // This would be a nicer solution, but it doesn't work in Safari
-    // (navigator as any).clipboard.writeText(val)
+    (navigator as any).clipboard.writeText(val)
 
-    const selBox = document.createElement('textarea')
-    selBox.style.position = 'fixed'
-    selBox.style.left = '0'
-    selBox.style.top = '0'
-    selBox.style.opacity = '0'
-    selBox.value = val
-    document.body.appendChild(selBox)
-    selBox.focus()
-    selBox.select()
-    document.execCommand('copy')
-    document.body.removeChild(selBox)
+    // const selBox = document.createElement('textarea')
+    // selBox.style.position = 'fixed'
+    // selBox.style.left = '0'
+    // selBox.style.top = '0'
+    // selBox.style.opacity = '0'
+    // selBox.value = val
+    // document.body.appendChild(selBox)
+    // selBox.focus()
+    // selBox.select()
+    // document.execCommand('copy')
+    // document.body.removeChild(selBox)
   }
 }
