7b0d4450fb4ca95482ab7a2fb1f67b7b95ae234e
acharl <amadeo.charle108@gmail.com>
2021-09-28 15:11:22 +0200

    feat(): don't dismiss BeaconRequestPage after dismissing DryRunPreviewPage

--- src/app/pages/beacon-request/beacon-request.page.ts
+++ src/app/pages/beacon-request/beacon-request.page.ts
@@ -296,7 +296,7 @@ export class BeaconRequestPage implements OnInit {
                 signedTransaction: dryRunResponse!.signedTransaction
               }
             },
-            true
+            false
           )
         })
         .catch(error => console.error(error))
